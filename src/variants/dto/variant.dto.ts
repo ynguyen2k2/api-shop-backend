101e184d4324cd08d8178b14550719e28b2e6763
ynguyen2024tt <ynguyen2024tt@gmail.com>
2026-05-18 16:14:10 +0700

    refactor: remove variant module

--- src/variants/dto/variant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString } from 'class-validator'
-
-export class VariantDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  id: string
-}
