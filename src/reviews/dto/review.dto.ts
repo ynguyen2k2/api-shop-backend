b6334c278cfb95a15bb7fa35d6f51ef261ccd3d2
ynguyen2024tt <ynguyen2024tt@gmail.com>
2026-05-18 16:29:45 +0700

    refactor: remove reviews module

--- src/reviews/dto/review.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString } from 'class-validator'
-
-export class ReviewDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  id: string
-}
--- src/reviews/dto/update-review.dto.ts
+++
@@ -1,7 +0,0 @@
-// Don't forget to use the class-validator decorators in the DTO properties.
-// import { Allow } from 'class-validator';
-
-import { PartialType } from '@nestjs/swagger'
-import { CreateReviewDto } from './create-review.dto'
-
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
