63f5053831c2b9099918275e83affe7cb65de978
ynguyen2024tt <ynguyen2024tt@gmail.com>
2026-05-19 22:00:03 +0700

    refactor: remove cart items module

--- src/cart-items/dto/update-cart-item.dto.ts
+++
@@ -1,7 +0,0 @@
-// Don't forget to use the class-validator decorators in the DTO properties.
-// import { Allow } from 'class-validator';
-
-import { PartialType } from '@nestjs/swagger'
-import { CreateCartItemDto } from './create-cart-item.dto'
-
-export class UpdateCartItemDto extends PartialType(CreateCartItemDto) {}
