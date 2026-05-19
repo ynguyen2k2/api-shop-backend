5eaf65862a09390a26ff181532b738e65ae7d94a
ynguyen2024tt <ynguyen2024tt@gmail.com>
2026-05-19 21:59:30 +0700

    refactor: remove plural carts modules

--- src/carts/dto/create-cart.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { CartItemDto } from '~/cart-items/dto/cart-item.dto'
-import { UserDto } from '~/user/dto/user.dto'
-
-export class CreateCartDto {
-  @ApiProperty()
-  user: UserDto
-}
