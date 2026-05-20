17644ca73c95bb206107cb39aca579691596af4d
ynguyen2024tt <ynguyen2024tt@gmail.com>
2026-05-20 21:45:05 +0700

    refactor: remove inventories module

--- src/inventories/dto/update-inventory.dto.ts
+++
@@ -1,7 +0,0 @@
-// Don't forget to use the class-validator decorators in the DTO properties.
-// import { Allow } from 'class-validator';
-
-import { PartialType } from '@nestjs/swagger'
-import { CreateInventoryDto } from './create-inventory.dto'
-
-export class UpdateInventoryDto extends PartialType(CreateInventoryDto) {}
